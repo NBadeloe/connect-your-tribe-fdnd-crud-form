85482cd79e23e9d0e28f8f13d9fb2ad6c7da5ff0
beaupd <beau@patrickdekker.nl>
2022-02-28 16:57:03 +0100

    Crud class initialized

---
+++ scripts/CRUD.js
@@ -0,0 +1,33 @@
+class CRUD {
+    constructor(url = "https://tribe.api.fdnd.nl/v1/") {
+        this.url = url;
+    }
+
+    /**
+     * Method to create a entry through HTTP POST method
+     *
+     *
+     */
+    Create() {}
+
+    /**
+     * Method to get an entries through HTTP GET method
+     *
+     *
+     */
+    Read() {}
+
+    /**
+     * Method to update an entry through HTTP PATCH method
+     *
+     *
+     */
+    Update() {}
+
+    /**
+     * Method to delete an entry through HTTP DELETE method
+     *
+     *
+     */
+    Delete() {}
+}
